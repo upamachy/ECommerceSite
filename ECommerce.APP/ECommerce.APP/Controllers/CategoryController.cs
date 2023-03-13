ea872c194e50fdfc7b1e47e45b85ae745bc2ab27
Upama chowdhury <upamachy360@gmail.com>
2023-03-13 13:22:00 +0600

    Category CRUD operation done

--- ECommerce.APP/ECommerce.APP/Controllers/CategoryController.cs
+++ ECommerce.APP/ECommerce.APP/Controllers/CategoryController.cs
@@ -42,7 +42,7 @@ namespace ECommerce.APP.Controllers
 
                 if (isAdded)
                 {
-                    return View("Success");
+                    return RedirectToAction("List");
                 }
             }
            
@@ -56,10 +56,83 @@ namespace ECommerce.APP.Controllers
             //loosely tied approach
             //ViewBag.CategoryList = categoryList;
 
-            ViewData["CategoryList"] = categoryList;
+            //ViewData["CategoryList"] = categoryList;
 
+            //after creating CategoryListVM . we don't need viewData because now we have a precise model for display category list
+            CategoryListVM categoryListVM = new CategoryListVM()
+            {
+                Title = "Category Overview",
+                Description = "You can manage Category From this page.you can create, update and delete categories....",
+                CategoryList = categoryList.ToList()
+            };
+
+            return View(categoryListVM);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("List");
+            }
+            var category = _categoryRepository.GetById((int)id);
+            if (category == null)
+            {
+                return RedirectToAction("List");
+            }
+            var categoryEditVM = new CategoryEditVM()
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Code = category.Code
+            };
+            return View(categoryEditVM);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CategoryEditVM categoryEditVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var category = new Catagory()
+                {
+                    Id = categoryEditVM.Id,
+                    Name = categoryEditVM.Name,
+                    Code = categoryEditVM.Code
+                };
+
+                bool isUpdated = _categoryRepository.Update(category);
+                if (isUpdated)
+                {
+                    return RedirectToAction("List");
+                }
+            }
             return View();
         }
+        
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("List");
+            }
+            var category = _categoryRepository.GetById((int)id);
+            if (category == null)
+            {
+                return RedirectToAction("List");
+            }
+
+            bool isRemoved= _categoryRepository.Remove(category);
+
+            if (isRemoved)
+            {
+                return RedirectToAction("List");
+            }
+
+            return RedirectToAction("List");
+
+        }
+
         public string CategoryListCreated(CategoryCreate[] categories)
         {
             string data = "Category list created" + Environment.NewLine;
---
+++ ECommerce.APP/ECommerce.APP/Models/CategoryModel/CategoryListVM.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Models.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.APP.Models.CategoryModel
+{
+    public class CategoryListVM
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Catagory> CategoryList { get; set; }
+    }
+}
